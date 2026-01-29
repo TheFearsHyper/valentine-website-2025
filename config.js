2a7ad8537add079fcd192b56a5042a3616434a22
TheFearsHyper <44450051+TheFearsHyper@users.noreply.github.com>
2026-01-28 21:39:33 -0500

    Update secretAnswer and celebration message

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "I don't like you, I really hate you! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you hate me?",                          // For the love meter
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get your gift, Sushi and a flowerss!!!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 

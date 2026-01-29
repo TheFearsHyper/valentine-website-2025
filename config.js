79d0f55e526cc8f6733b59c30cacbcc41713f854
TheFearsHyper <44450051+TheFearsHyper@users.noreply.github.com>
2026-01-28 22:04:22 -0500

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I really hate you! ❤️"           // Secret hover message
+            secretAnswer: "I don't like you, I really love you! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you hate me?",                          // For the love meter
@@ -42,7 +42,7 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You hate me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, Sushi and a flowerss!!!",
+        message: "Now come get your gift, hugs and a kiss!!!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
